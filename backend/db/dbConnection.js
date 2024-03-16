946102246f0e1c11802a7a406f10da2bf4460772
ankitverma242001@gmail.com <ankitverma242001@gmail.com>
2024-03-16 22:01:55 +0530

    backend initialized

---
+++ backend/db/dbConnection.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const dbConnection = () => {
+    mongoose.connect(process.env.MONGO_URI, {
+        dbName: "JOBSEEKING "
+    }).then(() => {
+        console.log("database connected")
+    })
+        .catch((err) => {
+            console.log(`error occur in db connection: ${err}`)
+        })
+}
